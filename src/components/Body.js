e2d30a557154fd09484c6c4e0a101e64e0075501
Michael Cui <cuimichael500@gmail.com>
2020-08-13 11:17:54 -0500

    body for table is done, last part is getting axios into it

---
+++ src/components/Body.js
@@ -0,0 +1,43 @@
+import React, { useContext } from "react";
+import MainContext from "../utils/MainContext";
+
+const Body = () => {
+  const context = useContext(MainContext);
+
+
+  return (
+    <tbody>
+      
+      {context.developerState.filteredUsers[0] !== undefined && context.developerState.filteredUsers[0].name !== undefined ? (
+        context.developerState.filteredUsers.map(({ login, name, picture, phone, email}) => {
+          return (
+            <tr key={login.uuid}>
+              <td data-th="Image" className="align-middle">
+                <img
+                  src={picture.medium}
+                  alt={"profile image for " + name.first + " " + name.last}
+                  className="img-responsive"
+                />
+              </td>
+              <td data-th="Name" className="name-cell align-middle">
+                {name.first} {name.last}
+              </td>
+              <td data-th="Phone" className="align-middle">
+                {phone}
+              </td>
+              <td data-th="Email" className="align-middle">
+                <a href={"mailto:" + email} target="__blank">
+                  {email}
+                </a>
+              </td>
+            </tr>
+          );
+        })
+      ) : (
+        <></>
+      )}
+    </tbody>
+  );
+}
+
+export default Body;
